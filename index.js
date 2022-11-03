fc9583eed5c87b671eec937d34701eea7ba03319
Vivek Rajput <indvivekrajput@gmail.com>
2022-11-04 01:08:23 +0530

    dding authentication and authorization

--- index.js
+++ index.js
@@ -7,23 +7,51 @@ const {useModel} =require("./models/useModel")
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("homem page")
-})
+
+
+const authentication=(req,res,next)=>{
+    const token= req.headers?.authorization.split(" ")[1];
+    try{
+        var decoded= jwt.verify(token,"abcd12345")
+        req.body.email=decoded.email
+        next()
+    }
+    catch(err){
+        res.send("please login again")
+    }
+}
+
+const authorisation = (permittedrole) => {
+    return async (req, res, next) => 
+    {
+    const email = req.body.email
+    const user = await useModel.findOne({email : email})
+    const role = user.role;
+ 
+        if(permittedrole.includes(role)){
+            next()
+        }
+        else{
+            res.send("Not authorised")
+        }
+    }
+}
+
 
 
 
 
 
 app.post("/signup", async (req, res) => {
-    const {email, password} = req.body;
+    const {email, password,role} = req.body;
     bcrypt.hash(password, 5, async function(err, hashed_password) {
         if(err){
             res.send("Something went wrong, please signup later")
         }
         const new_user = new useModel({
             email : email,
-            password : hashed_password
+            password : hashed_password,
+            role:role
         })
         await new_user.save()
         res.send("Sign up successfull")
@@ -46,7 +74,15 @@ app.post("/login", async (req, res) => {
     });
     
 })
+app.get("/",authentication,(req,res)=>{
+    res.send("homem page")
+})
 
+
+app.post("/products/create", authentication, authorisation(["customer"]), async (req, res) => {
+    res.send("Product created")
+       
+})
 
 app.listen(8012, async()=>{
     try{
--- models/useModel.js
+++ models/useModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const UseSchema= new mongoose.Schema({
     email:String,
     password:String,
+    role:String,
 })
 
 const  useModel=mongoose.model("authuser",UseSchema)
